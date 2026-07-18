628f559d14961dcae5a91f60bab53def08378b7d
ungakadaidigital-ux <ungakadaidigital@gmail.com>
2026-07-18 09:49:37 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MeAndCoachApp from "./MeAndCoachApp.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MeAndCoachApp />
+  </React.StrictMode>
+);
+
