2568bad28980a7542a89facedc0b1731680002c4
ungakadaidigital-ux <ungakadaidigital@gmail.com>
2026-07-27 02:59:59 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MeAndCoachApp from "./MeAndCoachApp.jsx";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MeAndCoachApp />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
